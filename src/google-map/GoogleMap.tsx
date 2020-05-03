6e18a88212fb822e3f5143a01a68d29c5daa1894
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2020-05-03 18:24:57 +0200

    show the google map on the screen

--- src/google-map/GoogleMap.tsx
+++ src/google-map/GoogleMap.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './GoogleMap.css';
 
-const GoogleMap = () => {
-  return <div>Hello from the google map!</div>;
-};
+class GoogleMap extends Component {
+  private readonly googleMapRef: React.RefObject<HTMLDivElement>;
+
+  constructor(props: any) {
+    super(props);
+    this.googleMapRef = React.createRef<HTMLDivElement>();
+  }
+
+  componentDidMount(): void {
+    new google.maps.Map(this.googleMapRef.current as Element, {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
+  render() {
+    return (
+      <div id="map"
+           ref={this.googleMapRef}>
+      </div>
+    );
+  }
+}
 
 export default GoogleMap;
